3016b13f022de843b7e2f88d909c662694822737
Sebastian <mrseb10@gmail.com>
2024-05-11 12:25:43 +0200

    Model, DbContext, Migration

--- SleepTracker.API/Program.cs
+++ SleepTracker.API/Program.cs
@@ -1,15 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SleepTracker.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<SleepSessionsContext>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
@@ -17,6 +20,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
 

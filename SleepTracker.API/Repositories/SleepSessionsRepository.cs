1c77515719bab852da08c864d4b541c82c8d4abe
Sebastian <mrseb10@gmail.com>
2024-05-11 12:31:48 +0200

    Repository Pattern

---
+++ SleepTracker.API/Repositories/SleepSessionsRepository.cs
@@ -0,0 +1,34 @@
+﻿using SleepTracker.API.Data;
+using SleepTracker.API.Models;
+
+namespace SleepTracker.API.Repositories;
+
+public class SleepSessionsRepository(SleepSessionsContext context) : ISleepSessionsRepository
+{
+  private readonly SleepSessionsContext _context = context;
+
+  public Task AddSession(SleepSession session)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task DeleteSession(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<SleepSession>> GetAllSessions()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<SleepSession?> GetSessionById(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task UpdateSession(SleepSession session)
+  {
+    throw new NotImplementedException();
+  }
+}

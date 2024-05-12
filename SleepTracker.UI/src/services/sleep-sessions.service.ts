dc639582c37c9fcb5d02bbc0d082f05b1d53e06f
Sebastian <mrseb10@gmail.com>
2024-05-13 00:15:45 +0200

    Snackbar (can't customize)

--- SleepTracker.UI/src/services/sleep-sessions.service.ts
+++ SleepTracker.UI/src/services/sleep-sessions.service.ts
@@ -1,20 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { SleepSession } from '../models/SleepSession';
 import { url } from '../config/config';
+import { SnackbarService } from './snackbar.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SleepSessionsService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private snackbarService: SnackbarService,
+  ) {}
 
   getSessions(): Observable<SleepSession[]> {
     return this.http.get<SleepSession[]>(url);
   }
 
   deleteSession(session: SleepSession): Observable<SleepSession> {
-    return this.http.delete<SleepSession>(url + session.id);
+    return this.http
+      .delete<SleepSession>(url + session.id)
+      .pipe(tap(() => this.snackbarService.snackbarLog('Session deleted.')));
   }
 }

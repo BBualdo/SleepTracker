ca7d46e7f2e2288c9ed6deeaa6c4cac8bf3e475a
Sebastian <mrseb10@gmail.com>
2024-05-12 12:37:06 +0200

    Main Menu

--- SleepTracker.UI/src/app/app.routes.ts
+++ SleepTracker.UI/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: MainMenuComponent }];
